eefde0e277da8a566d81a00d0d0ae5a55583a56f
Daniel Tollenaar <daniel@d2hydro.nl>
2021-02-17 18:22:49 +0100

    beter filter + shape-export

--- hydrotools.py
+++ hydrotools.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Mon Sep 21 23:18:13 2020
 
@@ -9,11 +8,19 @@ import geopandas as gpd
 from delft3dfmpy import HyDAMO
 from pathlib import Path
 
-from shapely.geometry import Polygon, LineString, Point
+from shapely.geometry import LineString, Point
 from shapely.ops import snap
 
-
 hydamo = HyDAMO()
+
+ATTRIBUTES = ["crosssections",
+              "parametrised_profiles",
+              "bridges",
+              "culverts",
+              "weirs",
+              "gemalen",
+              "pumps"]
+
 
 def _filter(gdf, attribute_filter):
     if isinstance(attribute_filter, dict):
@@ -21,7 +28,7 @@ def _filter(gdf, attribute_filter):
             if not isinstance(value, list):
                 value = [value]
             gdf = gdf[gdf[key].isin(value)]
-            
+
         return gdf
     else:
        raise IOError('attribute_filter should be dictionary') 
@@ -31,6 +38,7 @@ def read_file(path,
               hydamo_attribute,
               index_col=None,
               attribute_filter=None,
+              keep_columns=None,
               column_mapping=None,
               z_coord=False
               ):
@@ -68,35 +76,36 @@ def read_file(path,
             gdf.rename(columns=column_mapping, inplace=True)
             
         # drop all columns not needed 
-        required_columns = getattr(hydamo,hydamo_attribute).required_columns
+        required_columns = getattr(hydamo,hydamo_attribute).required_columns.copy()
+
+        if keep_columns:
+           required_columns += [col.lower() for col in keep_columns]
         
         if hydamo_attribute =='crosssections':
-            required_columns += ['order', 'category']
             if z_coord:
-                required_columns += ['z']
-        
+                required_columns += ['z', 'order']
+
         drop_cols = [col for col in gdf.columns if not col in required_columns + ['geometry']]
-        
         if len(drop_cols) > 0:
             gdf = gdf.drop(drop_cols, axis=1)
         
         return gdf
-    
+
+
 def to_file(model, hydamo_attribute, length=False, path=Path('.')):
-    '''converts hydamo class to shape-file'''
-    
+    """Convert hydamo class to shape-file."""
     path = Path(path)
-    hydamo_class = getattr(model,hydamo_attribute)
-    data = {col:hydamo_class[col].values for col in hydamo_class.columns}
-    
+    hydamo_class = getattr(model, hydamo_attribute)
+    data = {col: hydamo_class[col].values for col in hydamo_class.columns}
+
     if length:
         data = data = {**data, 'length': hydamo_class['geometry'].length.values}
-        
+
     gpd.GeoDataFrame(data=data).to_file(path.joinpath(f'{hydamo_attribute}.shp'))
-    
+
+
 def snap_ends(gdf, tolerance, digits=None):
-    ''' function to snap all end-vertices of a GeoDataFrame with LineStrings within a specified tolerance'''
-    
+    """Snap all end-vertices within a specified tolerance."""
     sindex = gdf.sindex
     snapped = []
     for index, row in gdf.iterrows():
@@ -112,27 +121,52 @@ def snap_ends(gdf, tolerance, digits=None):
         geom = row['geometry']
         # round digits (optionally)
         if digits:
-           geom = LineString([[round(coord, ndigits=digits) for 
+            geom = LineString([[round(coord, ndigits=digits) for
                                coord in coords] for coords in geom.coords])
-        
+
         if not gdf_selec.empty:
             geom_coords = list(geom.coords)
-            
+
             for _, row_selec in gdf_selec.iterrows():
-                #unfortunately this line doesn't work (always):
-                #geom = snap(geom, row_selec['geometry'], snap_tolerance)
-                
-                #but this does
-                for dst_vert in [0,-1]:
-                    for src_vert in [0,-1]:
-                        geom_coords[dst_vert] = snap(Point(geom_coords[dst_vert]),
-                                                     Point(row_selec['geometry'].coords[src_vert]), 
-                                                     tolerance=1).coords[0]
-#                        print(f'{_}: {geom_coords[-1]} {row_selec["geometry"].coords[src_vert]}')
+                for dst_vert in [0, -1]:
+                    for src_vert in [0, -1]:
+                        geom_coords[dst_vert] = snap(
+                            Point(geom_coords[dst_vert]),
+                            Point(row_selec['geometry'].coords[src_vert]),
+                            tolerance=1).coords[0]
             geom = LineString(geom_coords)
         # write feature in original GeoDataFrame
         gdf.loc[index, 'geometry'] = geom
         # mark index as snapped
         snapped += [index]
-        
+
     return gdf
+
+
+def filter_model(model, attribute_filter={}):
+    """Filter a hydamo model on an attribute filter on branches."""
+    attribute_filter = {key.lower(): value for key, value in attribute_filter.items()}
+
+    drop_branches = []
+    for key, value in attribute_filter.items():
+        drop_branches += list(
+            model.branches.loc[
+                model.branches['hydromodel'] != 'Boezemmodel_v4'].index)
+
+    drop_branches = list(set(drop_branches))
+
+    model.branches = model.branches.loc[~model.branches.index.isin(drop_branches)]
+
+    for attribute in ATTRIBUTES:
+        hydamo_class = getattr(model, attribute)
+        hydamo_class.loc[~hydamo_class['branch_id'].isin(drop_branches)]
+
+    return model
+
+
+def export_shapes(model, path=Path('.')):
+    """Export a hydamo class to shape-files."""
+    path = Path(path)
+    path.mkdir(exist_ok=True)
+    for attribute in ATTRIBUTES:
+        to_file(model, attribute, length=False, path=path)
